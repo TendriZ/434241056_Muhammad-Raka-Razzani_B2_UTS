20c74e71f9d43d7d46a0e927fd0bcf506d2c954e
TendriZ <rakarazzani24@gmail.com>
2026-07-07 13:26:43 +0700

    fix: add 2 necessary column

--- schema.sql
+++ schema.sql
@@ -5,26 +5,27 @@ CREATE TABLE public.profiles (
   id uuid NOT NULL,
   name text NOT NULL,
   username text NOT NULL UNIQUE,
-  role text,
+  role text DEFAULT 'user'::text CHECK (role = ANY (ARRAY['user'::text, 'helpdesk'::text, 'admin'::text])),
   created_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-  CONSTRAINT profiles_pkey PRIMARY KEY (id)
+  CONSTRAINT profiles_pkey PRIMARY KEY (id),
+  CONSTRAINT profiles_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id)
 );
-
 CREATE TABLE public.tickets (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
   user_id uuid NOT NULL,
   title text NOT NULL,
   description text NOT NULL,
-  category text DEFAULT 'hardware'::text,
-  priority text DEFAULT 'medium'::text,
-  status text,
+  status text DEFAULT 'pending'::text CHECK (status = ANY (ARRAY['pending'::text, 'on_progress'::text, 'resolved'::text])),
   assigned_to uuid,
   image_url text,
   created_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
   updated_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-  CONSTRAINT tickets_pkey PRIMARY KEY (id)
+  category text DEFAULT 'hardware'::text,
+  priority text DEFAULT 'medium'::text,
+  CONSTRAINT tickets_pkey PRIMARY KEY (id),
+  CONSTRAINT tickets_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.profiles(id),
+  CONSTRAINT tickets_assigned_to_fkey FOREIGN KEY (assigned_to) REFERENCES public.profiles(id)
 );
-
 CREATE TABLE public.ticket_history (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
   ticket_id bigint NOT NULL,
@@ -32,5 +33,7 @@ CREATE TABLE public.ticket_history (
   action text NOT NULL,
   message text,
   created_at timestamp with time zone NOT NULL DEFAULT timezone('utc'::text, now()),
-  CONSTRAINT ticket_history_pkey PRIMARY KEY (id)
+  CONSTRAINT ticket_history_pkey PRIMARY KEY (id),
+  CONSTRAINT ticket_history_ticket_id_fkey FOREIGN KEY (ticket_id) REFERENCES public.tickets(id),
+  CONSTRAINT ticket_history_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.profiles(id)
 );
